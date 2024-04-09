d36158d209ca49e07b353861f19d6e123f78a531
maemon4095 <maemondecocted@gmail.com>
2024-04-09 10:40:26 +0900

    use extension

--- src/lib.rs
+++ src/lib.rs
@@ -4,27 +4,18 @@ mod connection;
 mod error;
 mod event_emitter;
 mod listener;
+mod plugin_builder_bin_ipc_extension;
 mod request_path;
 mod state;
 
-use channel_credentials::ChannelCredentials;
-use connection::Connection;
-use error::{InvalidMethodError, RequestPathError};
-use event_emitter::EventEmitter;
-use futures::SinkExt;
 use listener::Listener;
-use rand::Rng;
-use request_path::RequestPath;
-use state::State;
-use std::sync::{Arc, Mutex};
 use tauri::{
-    async_runtime,
-    http::{self, status::StatusCode, Request, Response},
     plugin::{Builder as PluginBuilder, TauriPlugin},
-    AppHandle, Runtime,
+    Runtime,
 };
 
 pub use channel::{Receiver, Sender};
+pub use plugin_builder_bin_ipc_extension::PluginBuilderBinIpcExtension;
 
 type Body = Vec<u8>;
 
@@ -37,17 +28,6 @@ type Body = Vec<u8>;
 // ```ts
 // const conn = await connect({ port: 80 });
 // ```
-
-macro_rules! get_connection_into {
-    ($c: ident; $id:ident, $key: ident -> $v: ident) => {
-        let $v = $c.get($id).ok_or(RequestPathError)?;
-        #[allow(unused_mut)]
-        let mut $v = $v.lock().unwrap();
-        if $v.key != $key {
-            return Err(RequestPathError.into());
-        }
-    };
-}
 
 pub struct Builder<R: Runtime> {
     inits: Vec<Box<dyn FnOnce(PluginBuilder<R>) -> PluginBuilder<R>>>,
@@ -71,7 +51,7 @@ impl<R: Runtime> Builder<R> {
     ) -> Self {
         let scheme = scheme.into();
         self.inits.push(Box::new(move |builder| {
-            register_bin_ipc_protocol(builder, scheme, listener)
+            builder.register_bin_ipc_protocol(scheme, listener)
         }));
 
         self
@@ -84,123 +64,4 @@ impl<R: Runtime> Builder<R> {
         }
         builder.build()
     }
-}
-/// register bin ipc protocol to plugin builder.
-/// for plugin internal ipc.
-pub fn register_bin_ipc_protocol<R: Runtime>(
-    builder: PluginBuilder<R>,
-    scheme: impl Into<String>,
-    listener: impl Listener<R>,
-) -> tauri::plugin::Builder<R> {
-    let scheme = scheme.into();
-    let state = State::new(scheme.clone(), listener);
-    builder.register_uri_scheme_protocol(scheme, move |app_handle, req| {
-        if req.method() != http::method::Method::POST {
-            return Err(InvalidMethodError.into());
-        }
-
-        let uri: http::Uri = req.uri().parse()?;
-        let path: RequestPath = uri.path().parse()?;
-        let body = req.body();
-        match path {
-            RequestPath::Push { id, key } => {
-                let connections = state.connections();
-                get_connection_into!(connections; id, key -> connection);
-                let body = body.clone();
-                async_runtime::block_on(async { connection.tx.send(body).await })?;
-                any_origin_response()
-                    .status(StatusCode::OK)
-                    .body(Vec::new())
-            }
-            RequestPath::Pop { id, key } => {
-                let connections = state.connections();
-                get_connection_into!(connections; id, key -> connection);
-                let result = connection.rx.try_next();
-                match result {
-                    Ok(Some(body)) => any_origin_response().status(StatusCode::OK).body(body),
-                    Ok(None) => any_origin_response()
-                        .status(StatusCode::NO_CONTENT)
-                        .body(Vec::new()),
-                    Err(_) => any_origin_response()
-                        .status(StatusCode::CONTINUE)
-                        .body(Vec::new()),
-                }
-            }
-            RequestPath::CloseDown { id, key } => {
-                let connections = state.connections();
-                get_connection_into!(connections; id, key -> connection);
-                connection.rx.close();
-                any_origin_response().body(Vec::new())
-            }
-            RequestPath::CloseUp { id, key } => {
-                let connections = state.connections();
-                get_connection_into!(connections; id, key -> connection);
-                connection.tx.close_channel();
-                any_origin_response().body(Vec::new())
-            }
-            RequestPath::Close { id, key } => {
-                let connections = state.connections();
-                get_connection_into!(connections; id, key -> connection);
-                connection.tx.close_channel();
-                connection.rx.close();
-                any_origin_response().body(Vec::new())
-            }
-            RequestPath::Connect => handshake(app_handle, &state, req),
-            RequestPath::CleanUp { id, key } => {
-                {
-                    // key validation
-                    let connections = state.connections();
-                    get_connection_into!(connections; id, key -> connection);
-                }
-                // disconnect event response. assert js side cleanup is done
-                cleanup(app_handle, &state, id, req)
-            }
-        }
-    })
-}
-
-fn any_origin_response() -> http::ResponseBuilder {
-    http::ResponseBuilder::new().header(http::header::ACCESS_CONTROL_ALLOW_ORIGIN, "*")
-}
-
-fn handshake<R: Runtime>(
-    app_handle: &AppHandle<R>,
-    state: &State<R>,
-    _req: &Request,
-) -> Result<Response, Box<(dyn std::error::Error)>> {
-    let mut connections = state.connections_mut();
-    let key = state.rng().gen();
-    let reservation = connections.reserve();
-    let id = reservation.id();
-    let on_send = {
-        let app = app_handle.clone();
-        let scheme = Arc::clone(&state.scheme);
-        move || {
-            let emitter = EventEmitter::new(&scheme, id);
-            emitter.emit_ready(&app).map_err(Into::into)
-        }
-    };
-    let (server, client) = channel::channel(on_send, 32, 32);
-    let handle = state.listener.listen(app_handle, id, server.0, server.1);
-    reservation.set(Mutex::new(Connection {
-        key,
-        handle,
-        tx: client.0,
-        rx: client.1,
-    }));
-
-    let credentials = ChannelCredentials { key, id };
-    return any_origin_response()
-        .status(StatusCode::OK)
-        .body(serde_json::to_vec(&credentials)?);
-}
-
-fn cleanup<R: tauri::Runtime>(
-    _app_handle: &AppHandle<R>,
-    state: &State<R>,
-    id: usize,
-    _req: &Request,
-) -> Result<Response, Box<(dyn std::error::Error)>> {
-    state.close(id).unwrap();
-    return any_origin_response().body(Vec::new());
 }
