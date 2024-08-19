399dc077ba77bc73694378161dc0bc3bacd0c08c
maemon4095 <maemondecocted@gmail.com>
2024-08-20 06:16:58 +0900

    sync

--- crates/msgpack-macro-impl/src/command/impl_msgpack_command.rs
+++ crates/msgpack-macro-impl/src/command/impl_msgpack_command.rs
@@ -6,7 +6,6 @@ pub fn gen(item_fn: &syn::ItemFn) -> TokenStream {
     let fn_name = &item_fn.sig.ident;
     let command_name = &fn_name;
     let command_name_str = command_name.to_string();
-    let payload_ty = gen_payload_ty(item_fn);
 
     quote! {
         impl<R: ::tauri::Runtime> TauriPluginBinIpcMessagePackCommand<R> for #command_name {
@@ -17,52 +16,10 @@ pub fn gen(item_fn: &syn::ItemFn) -> TokenStream {
                 app: ::tauri::AppHandle<R>,
                 payload: ::std::vec::Vec<u8>,
             ) -> Vec<u8> {
-                #payload_ty
 
-                let payload: Payload = ::rmp_serde::from_slice(&payload).unwrap();
-
-
+                // todo: Deserializerを実装して各引数をデシリアライズする形で実装する。
+                // デシリアライザは、rmp_serdeのでシリアライザを内部に持つ。
             }
         }
-    }
-}
-
-fn gen_payload_ty(item_fn: &syn::ItemFn) -> TokenStream {
-    let params_list = &item_fn.sig.inputs;
-
-    let fields: Punctuated<&syn::PatType, syn::Token![,]> = params_list
-        .iter()
-        .map(|p| match p {
-            syn::FnArg::Receiver(_) => unreachable!(),
-            syn::FnArg::Typed(p) => p,
-        })
-        .collect();
-
-    quote! {
-        #[derive(::serde::Deserialize)]
-        struct Payload {
-            #fields
-        }
-    }
-}
-
-#[cfg(test)]
-mod test {
-    use super::*;
-
-    #[test]
-    fn test() {
-        let item_fn: syn::ItemFn = syn::parse_quote! {
-            fn a(arg0: Option<usize>, arg1: usize)  {}
-        };
-
-        let token = gen_payload_ty(&item_fn);
-
-        let file: syn::File = syn::parse_quote! {
-            #token
-        };
-        let pretty = prettyplease::unparse(&file);
-
-        println!("{}", pretty)
     }
 }
--- crates/msgpack/src/lib.rs
+++ crates/msgpack/src/lib.rs
@@ -1,4 +1,5 @@
 pub use msgpack_macro::command;
+use rmp_serde::decode::ReadRefReader;
 
 pub trait TauriPluginBinIpcMessagePackCommand<R: tauri::Runtime> {
     const NAME: &'static str;
@@ -10,9 +11,80 @@ pub trait TauriPluginBinIpcMessagePackCommand<R: tauri::Runtime> {
     ) -> impl ::std::future::Future<Output = Vec<u8>>;
 }
 
+// 外部クレートの型なので、実装のコンフリクトが起きる。
+// traitを使った方法は使えない。型を見て関数ポインタを返すような仕様にするか？
+impl<R: tauri::Runtime> CommandArg<R> for tauri::AppHandle<R> {
+    fn from_command<'de>(
+        de: CommandDeserializer<'de, R>,
+    ) -> Result<Self, Box<dyn std::error::Error>> {
+        Ok(de.app_handle)
+    }
+}
+
+impl<T: serde::de::DeserializeOwned, R: tauri::Runtime> CommandArg<R> for T {
+    fn from_command<'de>(
+        de: CommandDeserializer<'de, R>,
+    ) -> Result<Self, Box<dyn std::error::Error>> {
+        todo!()
+    }
+}
+
+trait CommandArg<R: tauri::Runtime>: Sized {
+    fn from_command<'de>(
+        de: CommandDeserializer<'de, R>,
+    ) -> Result<Self, Box<dyn std::error::Error>>;
+}
+
+struct CommandDeserializer<'de, R: tauri::Runtime> {
+    app_handle: tauri::AppHandle<R>,
+    de: rmp_serde::Deserializer<ReadRefReader<'de, [u8]>>,
+}
+
+struct Payload<R: tauri::Runtime> {
+    app: tauri::AppHandle<R>,
+    arg0: usize,
+}
+
+impl<R: tauri::Runtime> Payload<R> {
+    fn de<'de>(
+        app_handle: tauri::AppHandle<R>,
+        de: rmp_serde::Deserializer<ReadRefReader<'de, [u8]>>,
+    ) {
+    }
+}
 #[command]
 fn a(x: usize) -> usize {
     x
+}
+
+fn b<'de>(mut de: rmp_serde::Deserializer<ReadRefReader<'de, [u8]>>) {
+    struct X {}
+
+    struct V {}
+
+    impl<'de> serde::de::Visitor<'de> for V {
+        type Value = X;
+
+        fn expecting(&self, formatter: &mut std::fmt::Formatter) -> std::fmt::Result {
+            formatter.write_str("struct")
+        }
+
+        fn visit_map<A>(self, mut map: A) -> Result<Self::Value, A::Error>
+        where
+            A: serde::de::MapAccess<'de>,
+        {
+            while let Some(k) = map.next_key::<&str>()? {
+                match k {
+                    "a" => {
+                        map.next_value::<usize>()?;
+                    }
+                    _ => panic!(),
+                }
+            }
+
+            todo!()
+        }
+    }
 }
 
 #[cfg(test)]
