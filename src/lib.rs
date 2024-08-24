c0c4172caefeac1d908da333708584723f66d62e
maemon4095 <maemondecocted@gmail.com>
2024-08-24 19:47:50 +0900

    feature

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,5 @@
 pub use core::{BinIpcHandler, BoxError, PluginBuilderBinIpcExtension};
+#[cfg(feature = "msgpack")]
 pub use msgpack;
+#[cfg(feature = "default-msgpack")]
 pub use msgpack::{bin_command, generate_bin_handler};
